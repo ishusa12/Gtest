e252935e89b1619cb0c2f274b6663a854aa7d7f9
ishusa12 <ishusa654@gmail.com>
2022-09-08 14:59:16 -0400

    Rest controller

---
+++ src/main/java/com/example/test/controller/Rest.java
@@ -0,0 +1,23 @@
+package com.example.test.controller;
+
+import com.example.test.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Rest {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greetingCostomize")
+    public Greeting greetingCost(@RequestParam(value = "name") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}

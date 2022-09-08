0f1e6cb9a1bb6cefc5956f5f2187431fb51fb874
ishusa12 <ishusa654@gmail.com>
2022-09-08 14:32:53 -0400

    Initial commit

---
+++ src/main/java/com/example/test/controller/Controller.java
@@ -0,0 +1,36 @@
+package com.example.test.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.test.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class Controller {
+
+        private static final String template = "Hello, %s!";
+        private final AtomicLong counter = new AtomicLong();
+
+        @GetMapping("/greeting")
+        public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+            return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        }
+        @GetMapping("/greetingCostomize")
+        public Greeting greetingCost(@RequestParam(value = "name") String name) {
+            return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
